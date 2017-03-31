917f4ab4ab9b0235037c0c6c8386ec3aaa36716f
priitpaluoja <priitpaluoja@gmail.com>
2017-03-31 20:13:24 +0300

    Fixed PDF download problem

--- src/main/java/ee/nipt/web/ResultsController.java
+++ src/main/java/ee/nipt/web/ResultsController.java
@@ -10,13 +10,14 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.mail.MessagingException;
@@ -147,6 +148,8 @@ public class ResultsController {
                        Principal principal,
                        HttpServletResponse response) {
         response.setContentType("application/pdf");
+        String reportName = dto.getSampleNumber();
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + reportName + ".pdf\"");
         try {
             resultsService.appendPDF(response, resultsService.getReport(principal, dto));
         } catch (IOException | DocumentException e) {
